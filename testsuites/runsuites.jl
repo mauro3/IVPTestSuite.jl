fec990ad3a0f0f210a9c1db8713cf1c90856b990
Mauro Werder <mauro_lc@runbox.com>
2016-06-15 15:16:41 +0200

    Ran testsuites again.  Similar results to a year ago.

--- testsuites/runsuites.jl
+++ testsuites/runsuites.jl
@@ -5,14 +5,14 @@ using IVPTestSuite
 
 abstols = 10.0.^(-5:-1:-13)
 reltols = abstols
-ntsteps = [10.^(1:5), 500_000]
+ntsteps = vcat(collect(10.^(1:5)), 500_000)
 
 totest = IVPTestSuite.tc_all
 # totest = similar(totest)
 # totest[:rober] = IVPTestSuite.tc_all[:rober]
 
-#include("suite_Sundials.jl")
-#include("suite_DASSL.jl")
+include("suite_Sundials.jl")
+include("suite_DASSL.jl")
 include("suite_ODE.jl")
 include("suite_ODE_fixedstep.jl")
 
--- testsuites/suite_ODE_fixedstep.jl
+++ testsuites/suite_ODE_fixedstep.jl
@@ -6,7 +6,12 @@ for (n,tc) in totest
     tstepss = [linspace(tc.tspan[1], tc.tspan[2], n) for n in ntsteps]
     for (solverfn,solver) in S.ODEsolvers
         if isapplicable(solver, tc) && !isadaptive(solver)
-            suite = TestSuite(tc, solver, tstepss)
+            if tc==totest[:bruss1d]
+                # 10^5 steps take more than 10 min to run, skip:
+                suite = TestSuite(tc, solver, tstepss[ntsteps.<50_000])
+            else
+                suite = TestSuite(tc, solver, tstepss)
+            end
             res[solver] = run_ode_testsuite(suite)
         end
     end
